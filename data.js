15edf0e28c7d6876222e13f07795a8ffcf501b43
Nithinpatel1845 <nithin4518@gmail.com>
2024-11-16 11:26:55 +0530

    msg

---
+++ data.js
@@ -0,0 +1,21 @@
+      alert("WELCOME");
+        
+
+        var a=23;
+        let b="NITHIN";
+        const c=18;
+        console.log(a,b,c);
+      
+
+        var d=10;
+        console.log(d,typeof(d));
+        var e="PATEL";
+        console.log(e,typeof(e));
+        var f=true;
+        console.log(f,typeof(f));
+        var g=false;
+        console.log(g,typeof(g));
+        var h=null;
+        console.log(h,typeof(h));
+        var i=undefined;
+        console.log(i,typeof(i));
